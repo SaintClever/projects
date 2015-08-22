337c1025face71a9a6883b1ac0f7ea5b784fe012
SaintClever <parchment@gmx.com>
2015-08-22 13:25:15 -0400

    insecure & autopublish removed. Methods created, and finalized

---
+++ spatit/client/common.js
@@ -0,0 +1,4 @@
+// Subscribe to publishing from server/common.js
+Meteor.subscribe('products');
+
+Meteor.subscribe('categories');
--- spatit/lib/collections.js
+++ spatit/lib/collections.js
@@ -8,3 +8,13 @@ Products = new Mongo.Collection("products");
 ProductsImages = new FS.Collection("ProductsImages", {
   stores: [new FS.Store.GridFS("ProductsImages")]
 });
+
+// This allows inserts and downloads of the images
+ProductsImages.allow({
+  insert: function(fileId, doc){
+    return true;
+  },
+  download: function(fileId, doc){
+    return true;
+  }
+});
