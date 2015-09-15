cb20e6c1a922d43a8f13e3da5087179e6a189dfa
SaintClever <parchment@gmx.com>
2015-09-15 17:17:31 -0400

    karm app routing adjusted

--- karmel/lib/router.js
+++ karmel/lib/router.js
@@ -1,4 +1,26 @@
-// Home
-Router.route('/', function () {
+// Everything gets pushed into these templates
+Router.configure({
+  layoutTemplate: 'layout',
+  // loadingTemplate: 'loading',
+  // notFoundTemplate: 'notFound' 
+});
+
+
+Router.map(function(){
+  // Home
+  this.route('/', {
+    path: '/',
+    template: 'layout'
+  });
+
+});
+
+
+//or
+
+/*
+//Home
+this.route('/', function () {
   this.layout('layout');
 });
+*/
