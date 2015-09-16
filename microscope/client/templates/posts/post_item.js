d87f18a1b99d3831cd6868d5269dd5708cc6642e
SaintClever <parchment@gmx.com>
2015-09-16 16:42:12 -0400

    spacebars.js added. post_item.js/html vote added...etc.

--- microscope/client/templates/posts/post_item.js
+++ microscope/client/templates/posts/post_item.js
@@ -6,9 +6,24 @@ Template.postItem.helpers({
     var a = document.createElement('a'); // create and store a href tag in var a
     a.href = this.url; // return the href tag with the entire URL
     return a.hostname; // return the hostname which has a href
+  },
+  upvotedClass: function() {
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+    } else {
+      return 'disabled';
+    }
   }
   // ,
   // commentsCount: function() {
   //   return Comments.find({postId: this._id}).count();
   // }
+});
+
+Template.postItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
 });
